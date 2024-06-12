f8db04a916d3ab7283e844aa81adefd983dd4fe6
cathycampwagemann <catycamp@gmail.com>
2024-06-11 23:10:51 -0400

    Update api.py

--- api.py
+++ api.py
@@ -69,4 +69,4 @@ def predict():
 
 if __name__ == '__main__':
     # Especifica el puerto en el que la aplicación Flask escuchará las solicitudes
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
